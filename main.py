a12406bcce755eb7354b37248b0abd6558ddd3ec
Katrijn15 <katrijngoossens20034@gmail.com>
2023-12-05 20:20:19 +0100

    ping

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 BOT_PREFIX = os.getenv('BOT_PREFIX')
 
-def run():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    bot = commands.Bot(command_prefix = BOT_PREFIX, intents = intents)
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = BOT_PREFIX, intents = intents)
+
+async def main():
 
     @bot.event
     async def on_ready():
         print(f'User: {bot.user} [ID: {bot.user.id}]')
+        bot.remove_command('help')
 
-    @bot.command()
-    async def ping(ctx):
-        await ctx.send('pong')
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                await bot.load_extension(f'cogs.{filename[:-3]}')
+                print(f"Loaded Cog: {filename[:-3]}")
 
-    bot.run(BOT_TOKEN)
-
-if __name__ == '__main__':
-    run()
+asyncio.run(main())
+bot.run(BOT_TOKEN)
